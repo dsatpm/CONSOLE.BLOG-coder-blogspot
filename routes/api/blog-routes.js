78afa20e5c1f101668f5b7cd021476d6c6a85b97
dsatpm <jeremiahjh@gmail.com>
2023-12-14 21:35:54 -0800

    merged routes and controllers

--- routes/api/blog-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const blogController = require('../../controllers/api/blog-controller');
-
-// Define API routes for blog-related functionalities
-router.get('/recent', blogController.getRecentBlogPosts);
-// Other blog-related API routes...
-
-module.exports = router;
--- routes/home-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const homeController = require('../controllers/home-controller');
-
-// Define routes for home-related functionalities
-router.get('/', homeController.renderHomePage);
-router.get('/login', homeController.renderLoginPage);
-router.get('/signup', homeController.renderSignupPage);
-
-module.exports = router;
