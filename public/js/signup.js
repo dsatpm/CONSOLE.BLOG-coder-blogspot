64d36c0719a8070d81b97d1b43b151703215fd88
dsatpm <jeremiahjh@gmail.com>
2023-12-15 18:45:03 -0800

    fixing bugs in handlebars to render data

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#usernameSignup').value.trim();
+  const password = document.querySelector('#passwordSignup').value.trim();
 
   if (username && password) {
     const response = await fetch('/api/users', {
@@ -24,5 +24,5 @@ async function signupFormHandler(event) {
 }
 
 document
-  .querySelector('#signup-form')
+  .querySelector('#signupForm')
   .addEventListener('submit', signupFormHandler);
