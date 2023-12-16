46dd5cf65709d94eeb579cd17f0981d3658bd811
dsatpm <jeremiahjh@gmail.com>
2023-12-15 18:23:09 -0800

    completed auth, helper utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
-const Handlebars = require('handlebars');
+module.exports = {
+  format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
 
-Handlebars.registerHelper('formatDate', function (date) {
-  const options = { year: 'numeric', month: 'short', day: 'numeric' };
-  return new Date(date).toLocaleDateString(undefined, options);
-});
+}
