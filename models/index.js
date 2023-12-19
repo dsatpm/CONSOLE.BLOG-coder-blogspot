c576419d392c2805a151dd39bc6a3acb52e0a41a
dsatpm <jeremiahjh@gmail.com>
2023-12-18 18:02:44 -0800

    heroku fix heroku stuff

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 //  Models to be exported to the server
-const Post = require('./people-post-text');
-const User = require('./people-blog-text');
-const Comment = require('./people-comment-text');
+const Post = require('./post');
+const User = require('./user');
+const Comment = require('./comment');
 
 // user has many posts
 User.hasMany(Post, {
